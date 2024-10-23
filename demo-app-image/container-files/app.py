f4a23bcc0d548f0a36e5b64ff7a628cd227dabf8
okzldogan <o.kizildogan@southpole.com>
2024-10-23 09:29:27 +0200

    add docker files

---
+++ demo-app-image/container-files/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello Penguins!"
+
+@app.route("/log")
+def log():
+    return "Hello Log"
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=5000)
